8fd6cef52d53d05f677920b6220d3d40713d443f
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-07 19:57:37 -0600

    Print de prueba

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ if __name__ == '__main__':
 
 
     reponse = requests.post(url, data= json.dumps(payload))
+
+    #JSON post se encarga de serializarlos
+    #data entonces nosotros lo serializamos
+
     print(reponse.url)
 
     if reponse.status_code == 200:
         print(reponse.content)
+        print("Hola")
