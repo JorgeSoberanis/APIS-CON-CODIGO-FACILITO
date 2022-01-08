61a0606700d9d1add693b8f13820b8c76fb77825
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-07 20:04:46 -0600

    Cambios del JSON principal

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import json
 if __name__ == '__main__':
     url = 'https://httpbin.org/post'
     payload = {'nombre': 'Jorge', 'Curso': 'Python', 'Nivel': 'Intermedio'}
+    headers = {'content-Type': 'Aplication/json', 'acces-token' : '12345'}
 
-
-    reponse = requests.post(url, data= json.dumps(payload))
+    reponse = requests.post(url, data= json.dumps(payload), headers = headers)
 
     #JSON post se encarga de serializarlos
     #data entonces nosotros lo serializamos
@@ -15,5 +15,6 @@ if __name__ == '__main__':
     print(reponse.url)
 
     if reponse.status_code == 200:
-        print(reponse.content)
-        print("Hola")
+        #print(reponse.content)
+        headers_response = reponse.headers
+        print(headers_response)
