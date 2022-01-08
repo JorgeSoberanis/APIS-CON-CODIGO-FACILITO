7e833623c0fdfc7be53e09d3a2f8532cc251bbf8
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-07 23:54:31 -0600

    New carpetw

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 
 
 if __name__ == '__main__':
-    url = 'https://httpbin.org/post'
+    url = 'https://httpbin.org/delete'
     payload = {'nombre': 'Jorge', 'Curso': 'Python', 'Nivel': 'Intermedio'}
     headers = {'content-Type': 'Aplication/json', 'acces-token' : '12345'}
 
-    reponse = requests.post(url, data= json.dumps(payload), headers = headers)
+    reponse = requests.delete(url, data= json.dumps(payload), headers = headers)
 
     #JSON post se encarga de serializarlos
     #data entonces nosotros lo serializamos
@@ -17,4 +17,5 @@ if __name__ == '__main__':
     if reponse.status_code == 200:
         #print(reponse.content)
         headers_response = reponse.headers
+        server = headers_response['server']
         print(headers_response)
