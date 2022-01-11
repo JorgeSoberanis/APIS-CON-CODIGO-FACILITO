6b64cf79d06930422336413a7d7a560ad22f85e8
JorgeSoberanis <jsoberanisbalam@gmail.com>
2022-01-10 23:11:13 -0600

    Ya se consume la Pokeapi conrrectamente

--- pokemon.py
+++ pokemon.py
@@ -1,9 +1,11 @@
+
 import requests
 
-    if __name__ == '__main__':
-        url = 'http://pokeapi.co/api/v2/pokemon-form/'
+def get_pokemons (url ='http://pokeapi.co/api/v2/pokemon-form/', offset = 0  ):
+        args = {'offset' : offset } if offset else {}
 
-        response =  requests.get(url)
+        response =  requests.get(url, params = args)
+
         if response.statutus_code == 200:
 
             payload = response.json()
@@ -11,5 +13,12 @@ import requests
 
             if results:
                 for pokemon in results:
-                    name = pokemon ['name']
+                    name = pokemon['name']
                     print (name)
+
+            next = raw_input ("Continuar listando? [Y/N]")
+            if next == 'y':
+                    get_pokemons(offset=offset + 20)
+if __name__ == ' __main__ ':
+        url = 'http://pokeapi.co/api/v2/pokemon-form/'
+        get_pokemons()
